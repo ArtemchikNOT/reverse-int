1d5e5be04976cbdac87ee0ff170b0c9196c51c5f
ArtemchikNOT <artemchuro@mail.ru>
2020-03-11 22:03:53 +0300

    second com

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	if (n<0) {
+		x=n*(-1);
+		x = x + "";
+		return x.split("").reverse().join("");
+} else {
+	n = n + "";
+	return n.split("").reverse().join(""); 
+}
 }
